3be580119eaeabeb5ada8ed23a6c94684dfccd31
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-05-11 10:37:27 -0300

    add new features + banking system finalization

---
+++ project-testes/src/main/java/bancario/GerenciaConta.java
@@ -0,0 +1,96 @@
+package bancario;
+
+import java.util.List;
+
+public class GerenciaConta {
+	
+	private List<ContaCorrente> contasDoBanco;
+
+	public GerenciaConta(List<ContaCorrente> contasDoBanco) {
+		this.contasDoBanco = contasDoBanco;
+	}
+
+	
+	//Retorna uma lista com todas as contas do banco.
+	
+	public List<ContaCorrente> getContasDoBanco() {
+		return contasDoBanco;
+	}
+
+	// Pesquisa por uma conta a partir do seu ID.
+	 
+	public ContaCorrente pesquisaConta (int idConta) {
+
+		for (ContaCorrente contaCorrente : contasDoBanco) {
+			if(contaCorrente.getId() == idConta)
+				return contaCorrente;
+		}
+		return null;
+	}
+	
+	
+	 // Adiciona uma nova conta à lista de contas do banco.
+
+	public void adicionaConta (ContaCorrente novaConta) {
+		this.contasDoBanco.add(novaConta);
+	}
+
+	
+	 // Remove conta da lista de contas do banco.
+
+	public boolean removeConta (int idConta) {
+		
+		boolean contaRemovida = false;
+		
+		for (int i = 0; i < contasDoBanco.size(); i++) {
+			ContaCorrente conta = contasDoBanco.get(i);
+			if(conta.getId() == idConta){
+				contasDoBanco.remove(i);
+				break;
+			}
+		}
+		
+		return contaRemovida;
+	}
+
+	
+	 // Informa se uma determinada conta está ativa ou não.
+
+	public boolean contaAtiva (int idConta) {
+		
+		boolean contaAtiva = false;
+		
+		for (int i = 0; i < contasDoBanco.size(); i++) {
+			ContaCorrente conta = contasDoBanco.get(i);
+			if(conta.getId() == idConta)
+				if(conta.isAtiva()){
+					contaAtiva = true;
+					break;
+				}
+		}
+		
+		return contaAtiva;
+	}
+	
+	/**
+	 * Transfere um determinado valor de uma conta Origem para uma conta Destino.
+	 * Caso não haja saldo suficiente, o valor não será transferido.
+	 */
+	
+	public boolean transfereValor (int idContaOrigem, double valor, int idContaDestino) {
+		
+		boolean sucesso = false;
+		
+		ContaCorrente contaOrigem = pesquisaConta(idContaOrigem);
+		ContaCorrente contaDestino = pesquisaConta(idContaDestino);
+		
+//		if(contaOrigem.getSaldo() >= valor){
+			contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+			contaOrigem.setSaldo(contaOrigem.getSaldo() - valor);
+			sucesso = true;
+//		}
+	
+		return sucesso;
+	}
+
+}
